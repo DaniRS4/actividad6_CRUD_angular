11207753044f2f9c19593289efb035389f687beb
Daniel Real Sánchez <danielillero@gmail.com>
2025-03-18 01:34:31 +0100

    Servicio e Interfaces creadas

--- UsersCRUD/src/app/services/users.service.ts
+++ UsersCRUD/src/app/services/users.service.ts
@@ -1,9 +1,12 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsersService {
+  private httpClient = inject(HttpClient)
+  private baseUrl: string = "https://peticiones.online/api/users"
 
-  constructor() { }
 }
