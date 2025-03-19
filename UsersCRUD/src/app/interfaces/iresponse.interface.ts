abad4525db2342ff9125260f53424218d941107f
Daniel Real Sánchez <danielillero@gmail.com>
2025-03-19 01:01:03 +0100

    Vista Home creada

--- UsersCRUD/src/app/interfaces/iresponse.interface.ts
+++ UsersCRUD/src/app/interfaces/iresponse.interface.ts
@@ -1,9 +1,9 @@
 import { IUser } from "./iuser.interface";
 
 export interface IResponse {
-    page: number,
-    per_page: number,
-    total: number,
-    total_pages: number,
-    results: IUser[]
+    page: number;
+    per_page: number;
+    total: number;
+    total_pages: number;
+    results: IUser[];
 }
--- UsersCRUD/src/app/services/users.service.ts
+++ UsersCRUD/src/app/services/users.service.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
+import { lastValueFrom, Observable } from 'rxjs';
+import { IResponse } from '../interfaces/iresponse.interface';
 
 
 @Injectable({
@@ -9,4 +11,11 @@ export class UsersService {
   private httpClient = inject(HttpClient)
   private baseUrl: string = "https://peticiones.online/api/users"
 
+  getAllObservable(): Observable<IResponse> {
+    return this.httpClient.get<IResponse>(this.baseUrl)
+  }
+
+  getAllPromise(): Promise<IResponse> {
+    return lastValueFrom(this.httpClient.get<IResponse>(this.baseUrl))
+  }
 }
